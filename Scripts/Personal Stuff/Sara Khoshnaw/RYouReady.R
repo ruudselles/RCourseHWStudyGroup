3bd43716017f2f1a4091be66551638fe0d3d728e
sarabeautyclinic <74201137+sarabeautyclinic@users.noreply.github.com>
2020-11-21 15:02:29 +0100

    x

--- Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
+++ Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
@@ -15,9 +15,20 @@ library(janitor)
 load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
 load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_WideFormatHashed.RData")
 
-#reorganizing tables long format-------
+#reorganizing name tables from long format-------
 cleannames_data_long <- clean_names(Example_LongFormat)
 
-#inspect the results
+#inspect the results-------
 view (cleannames_data_long)
 
+#create new data with re-organise collums-----
+select_data_long <- select(Example_LongFormat, behandeling, rounddescription, everything())
+
+#create new data with deleting a collum-----
+select_data_long <- select(select_data_long, -Patient.traject.ID)
+
+#create new data with pipe function %>%------
+data_long_clean <- Example_LongFormat %>%
+  clean_names() %>%
+  select(behandeling, rounddescription, everything ()) %>%
+  select(-patient_traject_id)
