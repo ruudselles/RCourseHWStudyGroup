06930fa71e132c5ba37198fd1e4636943dc10121
sarabeautyclinic <74201137+sarabeautyclinic@users.noreply.github.com>
2020-11-21 10:54:11 +0100

    change

--- Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
+++ Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
@@ -1,6 +1,11 @@
-#load packages ------------
+#load packages ---------
 library(tidyverse)
 library(here)
+library(janitor)
 
-#clear the workspace ---
+#clear the workspace ---------
 'rm(list = ls())'
+
+#load data
+load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
+load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_WideFormatHashed.RData")
