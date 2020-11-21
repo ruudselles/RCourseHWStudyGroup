178acf19b63e51dd74cf19b3de8a5a053ae127ea
sarabeautyclinic <74201137+sarabeautyclinic@users.noreply.github.com>
2020-11-21 18:16:10 +0100

    x

--- Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
+++ Scripts/Personal Stuff/Sara Khoshnaw/RYouReady.R
@@ -15,7 +15,7 @@ library(janitor)
 load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
 load("~/Documents/GitHub/RCourseHWStudyGroup/data/Example_WideFormatHashed.RData")
 
-#reorganizing name tables from long format-------
+#reorganizing names from long format-------
 cleannames_data_long <- clean_names(Example_LongFormat)
 
 #inspect the results-------
@@ -27,8 +27,31 @@ select_data_long <- select(Example_LongFormat, behandeling, rounddescription, ev
 #create new data with deleting a collum-----
 select_data_long <- select(select_data_long, -Patient.traject.ID)
 
-#create new data with pipe function %>%------
+#create new data with pipe function %>% (alles in1)------
 data_long_clean <- Example_LongFormat %>%
   clean_names() %>%
   select(behandeling, rounddescription, everything ()) %>%
   select(-patient_traject_id)
+
+#inspect data (krijg je data te zien)-----
+summary(Example_LongFormat)
+
+#Data sorted based on average pain---
+data_desc<- Example_LongFormat %>%
+  arrange(desc(vasPijnGemiddeld_1))%>%
+  select(vasPijnGemiddeld_1)
+
+#filter for female and males
+Female<- Example_LongFormat %>%
+  filter( Geslacht == 'F') %>%
+  arrange(desc(vasPijnGemiddeld_1))%>%
+  select(vasPijnGemiddeld_1)
+
+Male<- Example_LongFormat %>%
+  filter( Geslacht == 'M') %>%
+  arrange(desc(vasPijnGemiddeld_1)) %>%
+  select(vasPijnGemiddeld_1)
+
+Test<- Example_LongFormat %>%
+  mutate(vanPijnGemiddeld_50=vasPijnGemiddeld_1>50)
+    
