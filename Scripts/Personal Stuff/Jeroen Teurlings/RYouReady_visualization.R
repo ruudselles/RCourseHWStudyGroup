be3d7c37870cb2dd6545361ba36b095e5bdbf550
JeroenTeurlings <75751875+JeroenTeurlings@users.noreply.github.com>
2020-12-15 20:04:31 +0100

    Update RYouReady_visualization.R

--- Scripts/Personal Stuff/Jeroen Teurlings/RYouReady_visualization.R
+++ Scripts/Personal Stuff/Jeroen Teurlings/RYouReady_visualization.R
@@ -62,3 +62,31 @@ Example_LongFormat%>%
   ggplot(aes(x=Geslacht, y=vasPijnGemiddeld_1, colour = Geslacht)) +
   geom_jitter() +
   facet_wrap(~ rounddescription)
+
+
+Example_LongFormat %>%
+  na.omit()%>%
+  group_by(Geslacht)%>%
+  summarize(mean = mean(vasPijnGemiddeld_1),
+            sd = sd(vasPijnGemiddeld_1),
+            n = n(),
+            stderr = sd/sqrt(n))%>%
+  ggplot(aes(x=Geslacht, y = mean)) +
+  geom_col()+
+  geom_errorbar(aes(x=Geslacht, ymin = mean-stderr, ymax = mean+stderr))
+
+
+Example_LongFormat %>%
+  na.omit()%>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1)) + 
+  geom_point()+
+  geom_smooth()
+
+
+Example_LongFormat %>%
+  na.omit()%>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1, colour = Geslacht)) +
+  geom_point() +
+  geom_smooth() +
+  theme_minimal()
+
