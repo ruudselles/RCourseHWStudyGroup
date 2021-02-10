62fb1a4ff925e2e63e19622e56fb5ea562269760
Lisaesteban <78275776+Lisaesteban@users.noreply.github.com>
2021-02-10 21:58:53 +0100

    CleanItUp1

--- Scripts/Personal Stuff/Lisa Esteban Lopez/RYouReady Clean it up.R
+++ Scripts/Personal Stuff/Lisa Esteban Lopez/RYouReady Clean it up.R
@@ -28,3 +28,24 @@ cleannames_data_long <- clean_names(Example_LongFormat)
 #Inspect results -----
 
 view(cleannames_data_long)
+
+# New data frame with behandeling, rounddescription and everything else -------
+
+select_data_long <- select(Example_LongFormat, behandeling, rounddescription, everything())
+
+# Remove from new data frame variable patient.traject.id -----------
+
+select_data_long <- select(select_data_long, -Patient.traject.id)
+
+# View results -----------
+view(select_data_long)
+
+# Clean it up with Pipes -----
+
+data_long_clean <- Example_LongFormat %>%
+  clean_names() %>%
+  select(behandeling, rounddescription, everything()) %>%
+  select(-patient_traject_id)
+
+  
+  
