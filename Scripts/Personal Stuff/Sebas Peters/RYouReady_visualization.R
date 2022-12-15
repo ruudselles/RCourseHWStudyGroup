b76125b4ae0c00925ee56cf0c53afb75cfa9fbba
Sebas-Peters <118528996+Sebas-Peters@users.noreply.github.com>
2022-12-15 20:19:33 +0100

    Einde VizWhiz

--- Scripts/Personal Stuff/Sebas Peters/RYouReady_visualization.R
+++ Scripts/Personal Stuff/Sebas Peters/RYouReady_visualization.R
@@ -8,10 +8,13 @@ library(janitor)
 library(ggbeeswarm)
 library(readr)
 
+#Set theme--------
+theme_set(theme_classic())
+
 #Load data -----------
 load("~/Library/Mobile Documents/com~apple~CloudDocs/GitHub/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
 
-#create scatterplot VAS pain average man/female
+#create scatterplot VAS pain average man/female-----
 Example_LongFormat %>%
   ggplot(aes(y = vasPijnGemiddeld_1, x = Geslacht)) +
   geom_point()
@@ -47,4 +50,67 @@ Example_LongFormat %>%
   facet_wrap(~ rounddescription)
 
 
-#Assignment: %>% the plot
+#Assignment: %>% the plot --> fout in code op Rcourse------
+Example_LongFormat %>%
+  filter(vasPijnGemiddeld_1 < 100) %>%
+  filter(vasPijnGemiddeld_1 > 0) %>%
+  ggplot(aes(y = vasPijnGemiddeld_1, x = Geslacht, color = Geslacht)) +
+  geom_jitter() + facet_wrap(~ rounddescription)
+
+#save plot------
+ggsave("VASpain_ggsave.png")
+
+#Histogram --> VizWhiz lesson 2 buiten Rcourse nog wel een keer doen-----
+
+##Make a plot, analogous to  the video, with the average VAS pain for men 
+#and women, and with error bars for the standard error.
+Example_LongFormat %>%
+  na.omit() %>%
+  group_by(Geslacht) %>%
+  summarise(mean = mean(vasPijnGemiddeld_1),
+                        sd = sd(vasPijnGemiddeld_1),
+                        n = n(),
+                        stderr = sd/sqrt(n)) %>%
+  ggplot(aes(x = mean, y = Geslacht)) +
+  geom_col() +
+  geom_errorbar(aes(y = Geslacht, xmin = mean-stderr, xmax = mean+stderr))
+
+#Make a scatter plot in which you can show to what extent VAS pain correlates with VAS function.----
+Example_LongFormat %>%
+  na.omit() %>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1)) +
+  geom_point() +
+  geom_smooth()
+
+#Add color
+Example_LongFormat %>%
+  na.omit() %>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1, color = Geslacht)) +
+  geom_point() +
+  geom_smooth()
+
+#Graph layout----
+Example_LongFormat %>%
+  na.omit() %>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1, color = Geslacht)) +
+  geom_point() +
+  geom_smooth() +
+  theme_classic()
+
+Example_LongFormat %>%
+  na.omit() %>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1, color = Geslacht)) +
+  geom_point() +
+  geom_smooth() +
+  theme_minimal()
+
+#add titles and change axis tabels ----
+Example_LongFormat %>%
+  na.omit() %>%
+  ggplot(aes(x = vasPijnGemiddeld_1, y = vasFunctie_1, color = Geslacht)) +
+  geom_point() +
+  geom_smooth() +
+  theme_classic() +
+  labs(title = "relationship between pain and hand functon",
+       x = "VAS pain (0-10)", 
+       y = "VAS function (0-10)")
