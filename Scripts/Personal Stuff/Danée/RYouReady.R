34a7f50f244e87f7f34ea0c7322224a2c3614805
grarends <g.arends@erasmusmc.nl>
2025-02-21 07:38:44 +0100

    Update

--- Scripts/Personal Stuff/Danée/RYouReady.R
+++ Scripts/Personal Stuff/Danée/RYouReady.R
@@ -1,1 +1,10 @@
-#Test
+#clear the workspace
+rm(list = ls())
+
+#load packages
+library(tidyverse)
+library(here)
+
+#get data
+load("~/Erasmus/Github data dingen_Timformatie/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
+load("~/Erasmus/Github data dingen_Timformatie/RCourseHWStudyGroup/data/Example_WideFormatHashed.RData")
