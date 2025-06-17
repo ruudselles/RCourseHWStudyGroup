f2850c6ea9987252d2f14dd20b661928c8de62cb
grarends <122599428+grarends@users.noreply.github.com>
2025-06-17 13:31:33 +0200

    Update RYouReady.R

--- Scripts/Personal Stuff/Danée/RYouReady.R
+++ Scripts/Personal Stuff/Danée/RYouReady.R
@@ -4,7 +4,14 @@ rm(list = ls())
 #load packages
 library(tidyverse)
 library(here)
+library(janitor)
 
 #get data
 load("~/Erasmus/Github data dingen_Timformatie/RCourseHWStudyGroup/data/Example_LongFormatHashed.RData")
 load("~/Erasmus/Github data dingen_Timformatie/RCourseHWStudyGroup/data/Example_WideFormatHashed.RData")
+
+#clean up the data
+cleannames_data_long <- clean_names(Example_LongFormat)
+view(cleannames_data_long)
+select_data_long <- select(cleannames_data_long, behandeling, rounddescription, everything())
+select_data_long <- select(cleannames_data_long, -patient_traject_id)
